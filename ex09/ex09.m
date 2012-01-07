9b6be53d02447fc7a1c6c878ffaa8271b3fbd549
Rolf Schroeder <rolf.schr@gmail.com>
2012-01-07 14:26:52 +0100

    added kmeans() for 9.3

--- ex09/ex09.m
+++ ex09/ex09.m
@@ -1,14 +1,33 @@
 #! /usr/bin/octave -qf
 
 
+#global DATA = [
+#-1 -1 -1
+#2 2 1
+#-3 -3 -1
+#1 2 1
+#1 3 1
+#1 4 1
+#];
 global DATA = [
--1 -1 -1
-2 2 1
--3 -3 -1 
-1 2 1
-1 3 1
-1 4 1
-];
+1 1
+1 3
+2 2
+3 1
+3 3
+#####
+1 -1
+1 -2
+1.5 -1.5
+2 -1
+2 -2
+######
+-3 1
+-3 2
+-2.5 1.5
+-2 1
+-2 2
+]
 
 # measure distance between two data points
 function _distance = distance(p1, p2)
@@ -42,4 +61,47 @@ function _knn = knn(point, k)
 	
 endfunction
 
-knn([5;5], 3)
+
+# 9.2 Parzen Windows
+function _pn = pn(point)
+
+
+endfunction
+
+
+
+# 9.3 RBF Network
+
+# calculate the k centroids
+function _centroids = kmeans(k, DATA)
+	n = length(DATA);
+	# pick random data points as initial centroids
+	t = [];
+	for i = 1:k
+		index = unidrnd(n);
+		t = [t, get_point(DATA, index)];
+	endfor
+	# update cenroids
+	for i = 1:(10 * n)
+		index = unidrnd(n);
+		# choose data point
+		x = get_point(DATA, index);
+
+		# determine closest centroid
+		distances = [];
+		for j = 1:length(t)
+			distances = [distances; distance(x, t(:,j)), j];
+		endfor
+		distances = sortrows(distances);
+		jnearest = distances(1,2);
+		nearest = t(:,jnearest);
+
+		# update centroid
+		nearest = nearest + (1 / n) * (x - nearest);
+		t(:,jnearest) = nearest;
+	endfor
+	_centroids = t;
+endfunction
+
+kmeans(3, DATA);
+
