d75924e195dace8e5bc259e9f030c7fbf6484109
Robin Vobruba <hoijui@mailbox.tu-berlin.de>
2012-01-09 15:03:12 +0100

    ex09: add sample generation and plot

--- ex09/ex09.m
+++ ex09/ex09.m
@@ -29,6 +29,71 @@ global DATA = [
 -2 2
 ]
 
+
+global mu = [0, 1; 1, 0; 0, 0; 1, 1];
+global sigma = sqrt(0.1);
+global nSampPerClass = 40;
+
+# Generate samples
+function _samples = generateSamples()
+	global mu;
+	global sigma;
+	global nSampPerClass;
+	_samples = [];
+	samplesC1X = [];
+	samplesC1Y = [];
+	samplesC2X = [];
+	samplesC2Y = [];
+
+	subC1 = [[], []; [], []];
+	subC2 = [[], []; [], []];
+	for p = 1:2
+		for d = 1:2
+			myMu = mu(p, d);
+			for s = 1:nSampPerClass
+				subC1(p, d, s) = myMu + sigma*randn;
+			endfor
+		endfor
+	endfor
+	for p = 1:2
+		for d = 1:2
+			myMu = mu(2+p, d);
+			for s = 1:nSampPerClass
+				subC2(p, d, s) = myMu + sigma*randn;
+			endfor
+		endfor
+	endfor
+
+	for s = 1:nSampPerClass
+		ci = 1 + mod(s, 2);
+		x = subC1(ci, 1, s);
+		y = subC1(ci, 2, s);
+		samplesC1X(end+1) = x;
+		samplesC1Y(end+1) = y;
+	endfor
+
+	for s = 1:nSampPerClass
+		ci = 1 + mod(s, 2);
+		x = subC2(ci, 1, s);
+		y = subC2(ci, 2, s);
+		samplesC2X(end+1) = x;
+		samplesC2Y(end+1) = y;
+	endfor
+
+	_samples = [samplesC1Y; samplesC1X; samplesC2Y; samplesC2X];
+endfunction
+
+mySamples = generateSamples();
+
+hold on
+plot(mySamples(1,:), mySamples(2,:), 'r*');
+plot(mySamples(3,:), mySamples(4,:), 'b*');
+hold off
+title("Verteilung")
+legend(["C1"; "C2"]);
+print("initial.png", "-dpng")
+
+
 # measure distance between two data points
 function _distance = distance(p1, p2)
 	_distance = norm(p1-p2);
